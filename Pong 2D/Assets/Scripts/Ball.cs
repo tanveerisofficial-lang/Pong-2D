90910d957ee1dd5a1af853d4b3ab8c96fd98510a
tanveerisofficial-lang <tanveerisofficial@gmail.com>
2026-01-07 06:49:42 +0530

    Script Ball and references

---
+++ Pong 2D/Assets/Scripts/Ball.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float startingSpeed = 5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
