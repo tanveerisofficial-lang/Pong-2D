3b5eb75c47043a0d6d3025da72b6e9c929751b6e
tanveerisofficial-lang <tanveerisofficial@gmail.com>
2026-01-07 06:56:49 +0530

    Ball is scripted

--- Pong 2D/Assets/Scripts/Ball.cs
+++ Pong 2D/Assets/Scripts/Ball.cs
@@ -7,7 +7,14 @@ public class Ball : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        bool isRight = UnityEngine.Random.value > 0.5f;
+        float xVelocity = -1f;
+        if (isRight==   true)
+        {
+            xVelocity = 1f;
+        }
+        float yVelocity = UnityEngine.Random.Range(-1f, 1f);
+        rb.linearVelocity = new Vector2(xVelocity, yVelocity).normalized * startingSpeed;
     }
 
     // Update is called once per frame
